cc930a19a7969c714d5db0bf953008e737f2e27e
Nick Schneider <n.schneider@tum.de>
2018-02-26 16:53:16 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,13 @@
 Rails.application.routes.draw do
-  get 'organizations/new'
-
-  get 'organizations/create'
-
-  get 'organizations/update'
-
-  get 'organizations/edit'
-
-  get 'organizations/show'
-
-  get 'applications/new'
-
-  get 'applications/create'
-
-  get 'applications/show'
-
-  get 'applications/index'
-
-  get 'applications/edit'
-
-  get 'applications/upgrade'
-
-  get 'projects/new'
-
-  get 'projects/create'
-
-  get 'projects/show'
-
-  get 'projects/index'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/show'
 
   devise_for :organizations
   devise_for :users
   root to: 'pages#home'
+
+  resources :applications, only: [:new, :create, :show, :index, :edit, :update]
+  resources :organizations, only: [:new, :create, :show, :edit, :update]
+  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :projects
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
