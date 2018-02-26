ea78fb18d6b0ce4c1fcf4b549a1e7432e65ebe78
Nick Schneider <n.schneider@tum.de>
2018-02-26 18:31:30 +0100

    seeds finished and photouploader added

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,83 @@ require 'faker'
 Application.destroy_all
 Project.destroy_all
 Organization.destroy_all
-Team_member.destroy_all
+TeamMember.destroy_all
 Team.destroy_all
 User.destroy_all
+
+p "Creating organizations"
+
+20.times do
+  organization = Organization.new(
+    email: Faker::Internet.email,
+    location: "#{Faker::Address.street_address}, #{Faker::Address.zip_code},
+     #{Faker::Address.state}, #{Faker::Address.country}",
+     website: Faker::Internet.url,
+     description: Faker::Lorem.paragraph,
+     name: Faker::Company.name
+    )
+  organization.remote_photo_url = "https://source.unsplash.com/weekly?company"
+  organization.save
+
+end
+
+p "Creating projects"
+
+
+project_titles = ["Google Maps integration", "Complete Website", "Sign Up", "Payement integration", "Refactoring Website", "Make our Website responsive"]
+status = ["Pending", "Confirmed by the team", "Accepted by the organization", "Declined by organization", "Rejected by the team"]
+20.times do
+ project = Project.new(
+    title: project_titles.sample,
+    description: Faker::Lorem.paragraph,
+    budget: (100..40_000).to_a.sample,
+    due_date: Faker::Date.forward(100),
+    status: status.sample
+    )
+  project.remote_photo_url = "http://res.cloudinary.com/jules/image/upload/v1519664939/seeds/#{(1..20).to_a.sample}"
+  project.save
+end
+
+p "Creating teams"
+
+20.times do
+  Team.create(about_us: Faker::Lorem.paragraph)
+end
+
+p "Creating user"
+
+20.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: "password123",
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    github: "https://github.com/Jb5322",
+    pic_url: "https://source.unsplash.com/person",
+    about_me: "#{Faker::SiliconValley.motto} + #{Faker::SiliconValley.quote}"
+    )
+
+end
+
+p "Creating team member"
+
+
+20.times do
+  TeamMember.create(
+    lead_dev: [true, false].sample,
+    team: Team.all.sample,
+    user: User.all.sample
+    )
+end
+
+p "Creating applications"
 
 
 20.times do
   Application.create(
     pitch: Faker::Lorem.paragraph,
     accepted: [true, false].sample,
-    user:
+    team: Team.all.sample,
+    project: Project.all.sample
     )
-
 end
