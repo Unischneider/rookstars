484932f77a0563cd5d0f5e48b32c4f3b939bc667
Juliette Chevalier <juliettech.1393@gmail.com>
2018-02-26 16:05:40 +0100

    database migrated

--- db/migrate/20180226145111_add_columns_to_users.rb
+++ db/migrate/20180226145111_add_columns_to_users.rb
@@ -1,10 +1,10 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, first_name, string
-    add_column :users, last_name, string
-    add_column :users, github, string
-    add_column :users, linkedin, string
-    add_column :users, pic_url, string
-    add_column :users, about_me, text
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :github, :string
+    add_column :users, :linkedin, :string
+    add_column :users, :pic_url, :string
+    add_column :users, :about_me, :text
   end
 end
