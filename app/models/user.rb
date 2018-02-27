52c4db8a59a28c6ef87eca98795f4fe85026f28e
Nick Schneider <n.schneider@tum.de>
2018-02-27 12:23:12 +0100

    resolved route problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,15 @@ class User < ApplicationRecord
   mount_uploader :pic_url, PhotoUploader
 
 
+
+def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
