64ac34a4ae89ae3383f59b849955bf36173a904f
Nick Schneider <n.schneider@tum.de>
2018-02-27 13:12:24 +0100

    sign in with github possible

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,10 @@ def self.new_with_session(params, session)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
+      if auth.info.name
+        user.first_name = auth.info.name.split.first   # assuming the user model has a name
+        user.last_name = auth.info.name.split.last
+      end
       user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :applications, only: [:new, :create, :show, :index, :edit, :update]
   resources :organizations, only: [:new, :create, :show, :edit, :update]
-  resources :users, only: [:new, :create, :show, :edit, :update]
+  # resources :users, only: [:new, :create, :show, :edit, :update]
   resources :projects
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
