ec8dfeb77ccc87744f65bfb19ab5a232ffc86ffa
Nick Schneider <n.schneider@tum.de>
2018-02-28 12:41:34 +0100

    make it possible to create a new project as a user

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,7 +13,7 @@ class ProjectsController < ApplicationController
 
   def new
     @project = Project.new
-    @application = Application.new
+    @proposal = Proposal.new
     authorize @project
   end
 
