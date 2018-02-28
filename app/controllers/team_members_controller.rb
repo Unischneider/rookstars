a5bbbb5794a3d16c3f610d09fa8048546f182f13
Juliette Chevalier <juliettech.1393@gmail.com>
2018-02-28 16:47:15 +0100

    added team_member form

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -1,9 +1,28 @@
 class TeamMembersController < ApplicationController
   def new
-    @team = Team.new
+    @team = Team.find(params[:team_id])
     @team_member = TeamMember.new
+    authorize @team_member
   end
 
   def create
+    @team = Team.find(params[:team_id])
+    @team_member = TeamMember.new(set_params)
+    authorize @team_member
+    if @team_member.save
+      render team_path(@team)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:about_us)
+  end
+
+  def set_params
+    params.require(:team_member).permit(:user_id, :team_id, :lead_dev)
   end
 end
