0964c45d14708f7db6049f1438761f76ec10396f
Nick Schneider <n.schneider@tum.de>
2018-02-28 14:40:02 +0100

    save

---
+++ app/controllers/applications_controller.rb
@@ -0,0 +1,19 @@
+class ApplicationsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
