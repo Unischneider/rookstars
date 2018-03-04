232328809f4e0bf2e9bea1a531f1ff377492c388
Juliette Chevalier <juliettech.1393@gmail.com>
2018-03-04 15:32:03 +0100

    added path to creating team members

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,7 +5,7 @@ class Team < ApplicationRecord
   has_many :projects, through: :proposals
 
 
-  def lead_dev
+  def lead_dev_finder
     team_member = team_members.find { |team_member| team_member.lead_dev }
     if team_member
      team_member.user.first_name
@@ -20,7 +20,19 @@ class Team < ApplicationRecord
     #  end
   end
 
+  # def teams_members_finder
+  #   members = self.team_members.map{ |member| member.user }
+  #   # members = self.team_members.where{ |member| member.team_id == self.id }
+  #   # team_members = TeamMember.all
+  #   # team_members.where(lead_dev: true && team_members.team_id == self.id)
+  # end
+
+  # def members_full_name
+  #   teams_members_finder
+  #   name = "#{teams_members_finder.first_name} #{teams_members_finder.last_name}"
+  # end
+
   def lead_dev_label
-    "#{lead_dev}'s team"
+    "#{lead_dev_finder}'s team"
   end
 end
