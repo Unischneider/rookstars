575b360b3bfdae9dbee197951bd0434aad5a565a
Juliette Chevalier <juliettech.1393@gmail.com>
2018-03-06 16:53:11 +0100

    typed.js works with loop

--- app/javascript/packs/typed.js
+++ app/javascript/packs/typed.js
@@ -2,5 +2,11 @@ import Typed from 'typed.js';
 
 new Typed('#title-typed', {
   strings: ["saving the world. ^500", "protecting our oceans. ^500", "gender equality. ^500", "promoting human rights. ^500", "fighting climate change. ^500"],
-  typeSpeed: 50
+  typeSpeed: 50,
+  /**
+   * @property {boolean} loop loop strings
+   * @property {number} loopCount amount of loops
+   */
+  loop: true,
+  loopCount: 50
 });
