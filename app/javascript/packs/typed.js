7bb3f610a5fe9d38b75b090a79857b0af30c2b2e
Juliette Chevalier <juliettech.1393@gmail.com>
2018-03-06 16:56:56 +0100

    sentences now all fit in one line

--- app/javascript/packs/typed.js
+++ app/javascript/packs/typed.js
@@ -1,7 +1,7 @@
 import Typed from 'typed.js';
 
 new Typed('#title-typed', {
-  strings: ["saving the world. ^500", "protecting our oceans. ^500", "gender equality. ^500", "promoting human rights. ^500", "fighting climate change. ^500"],
+  strings: ["saving the world. ^500", "protecting our oceans. ^500", "gender equality. ^500", "promoting human rights ^500", "fighting climate change. ^500"],
   typeSpeed: 50,
   /**
    * @property {boolean} loop loop strings
