91f7d12fa90d71a449e81bb91af0f52331918bf5
Eric Berner <john-eric.berner@t-online.de>
2018-03-06 17:20:05 +0100

    map not done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,13 @@ class PagesController < ApplicationController
   def home
     @projects = Project.all.first(6)
   end
+
+  def contact_us
+    @marker =
+      {
+        lat: 52.506972,
+        lng: 13.391486,
+      }
+
+  end
 end
