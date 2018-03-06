0ab82b20047f9ce2b0851c211b2285a7613ec285
Eric Berner <john-eric.berner@t-online.de>
2018-03-06 22:47:00 +0100

    contact page nearly done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,17 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :contact_us]
 
   def home
     @projects = Project.all.first(6)
   end
 
   def contact_us
-    @markers = {
+    @markers = [{
         lat: "52.506972",
         lng: "13.391486",
-
         icon: ActionController::Base.helpers.image_url('pointer.png')
-      }
+      }]
+
+    @projects = Project.all.first
   end
 end
