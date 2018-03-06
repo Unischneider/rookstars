8936e871c5df6f121bf509be3fcca88179e64091
Juliette Chevalier <juliettech.1393@gmail.com>
2018-03-06 16:41:04 +0100

    typed.js woooorking for header woo

---
+++ app/javascript/packs/typed.js
@@ -0,0 +1,6 @@
+import Typed from 'typed.js';
+
+new Typed('#title-typed', {
+  strings: ["saving the world. ^500", "protecting our oceans. ^500", "gender equality. ^500", "promoting human rights. ^500", "fighting climate change. ^500"],
+  typeSpeed: 50
+});
