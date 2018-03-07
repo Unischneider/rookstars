c2f2d9857c5f4e4d80683ff59a4005245d9e9a75
Nick Schneider <n.schneider@tum.de>
2018-03-07 14:37:49 +0100

    after_action on creation of user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,13 +1,16 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  after_action :add_photo, only: [:create]
   def create
     super
+  end
+
+  private
+  def add_photo
     if current_user.photo.file.nil?
       current_user.remote_photo_url = "http://res.cloudinary.com/jules/image/upload/v1520336238/placeholder.jpg"
       current_user.save
     end
   end
-
-  private
 
   def sign_up_params
     params.require(:user).permit(:first_name, :photo, :about_me, :email, :password, :password_confirmation)
