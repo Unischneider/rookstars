499736274652346b7a39db697c72010fc05cebaa
Martin de Frutos <martindfru@gmail.com>
2018-03-08 20:19:21 +0100

    fix: error message for contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     if @contact.deliver
       flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
     else
-      flash.now[:error] = 'Cannot send message.'
+      flash.now[:error] = 'Sorry, we could not send your message.'
       redirect_to root_path
     end
   end
