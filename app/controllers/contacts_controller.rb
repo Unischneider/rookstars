65ec2916cc392bd51aa0f7adc442bb344359bd04
Martin de Frutos <martindfru@gmail.com>
2018-03-08 20:28:10 +0100

    fix: contact controller request and routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,8 +6,11 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
+
+
     if @contact.deliver
       flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      redirect_to root_path
     else
       flash.now[:error] = 'Sorry, we could not send your message.'
       redirect_to root_path
