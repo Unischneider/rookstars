f9f805668a4c0f926b8df0f2b1a24482ae402bf6
Martin de Frutos <martindfru@gmail.com>
2018-03-08 20:18:43 +0100

    fix: broken redirect

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ class ContactsController < ApplicationController
       flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
     else
       flash.now[:error] = 'Cannot send message.'
-      render :new
+      redirect_to root_path
     end
   end
 end
