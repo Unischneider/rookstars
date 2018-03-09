617d4ad08e8720424c190ac1bb01ad9b14bf2eae
Nick Schneider <n.schneider@tum.de>
2018-03-09 12:02:22 +0100

    added

--- app/controllers/organizations/proposals_controller.rb
+++ app/controllers/organizations/proposals_controller.rb
@@ -1,5 +1,14 @@
 class Organizations::ProposalsController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def index
-    @proposals = Proposal.where(organization: current_organization)
+    # @proposals = Proposal.where(project: current_organization)
+    raise
+    authorize @proposals
+  end
+
+  private
+  def pundit_user
+    current_organization
   end
 end
---
+++ app/policies/organizations/proposal_policy.rb
@@ -0,0 +1,11 @@
+class Organizations::ProposalPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    true
+  end
+end
